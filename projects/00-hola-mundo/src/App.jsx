f596766c5911e42fceeab177fdb1aa7d5d1a29a9
GabrielChuchuca <gavriellalx@hotmail.com>
2023-08-04 01:36:56 -0500

    3nd commit

---
+++ projects/00-hola-mundo/src/App.jsx
@@ -0,0 +1,17 @@
+export function App () {
+    return (
+        <article style={{ display: 'flex', color: '#fff'}}>
+            <header>
+                <img src="https://png.pngtree.com/png-vector/20220817/ourmid/pngtree-cartoon-man-avatar-vector-ilustration-png-image_6111064.png" alt="" />
+                <div>
+                    <strong>midudev</strong>
+                </div>
+            </header>
+            <aside>
+                <button>
+                    Segir
+                </button>
+            </aside>
+        </article>
+    )
+}
--- projects/00-hola-mundo/src/main.jsx
+++ projects/00-hola-mundo/src/main.jsx
@@ -1,22 +1,13 @@
 
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
+import { App } from './App'
+import './index.css'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const createButton = ({text}) => {
-  return (
-    <button>
-      {text}
-    </button>
-  )
-}
+
 
 root.render(
-  <React.Fragment>
-    {createButton({ text: 'Button1' })}
-    {createButton({ text: 'Button2' })}
-    {createButton({ text: 'Button3' })}
-  </React.Fragment>
+  <App />
 )
