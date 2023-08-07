d5ba6cb384d215cd01dd3c0b00f5830d75174e9d
GabrielChuchuca <gavriellalx@hotmail.com>
2023-08-06 23:18:28 -0500

    Update 02

--- projects/00-hola-mundo/src/App.jsx
+++ projects/00-hola-mundo/src/App.jsx
@@ -1,13 +1,39 @@
 import './App.css'
 import { TwitterFollowCard } from './TwitterFollowCard'
+
+const users = [
+    {
+        userName: 'alx',
+        name: 'Gabriel',
+        isFollowing: true
+    },
+    {
+        userName: 'pheralb',
+        name: 'Pablo H.',
+        isFollowing: false
+    },
+    {
+        userName: 'PacoHdezs',
+        name: 'Paco Hdez',
+        isFollowing: true
+    }
+]
 export function App() {
     //const format = (userName) => `@${userName}`
     
     return (
         //<article style={{ display: 'flex', alignItems: 'center', color: '#fff'}}>
         <section className='App'>
-            <TwitterFollowCard userName="alx">Gabriel</TwitterFollowCard>
-            <TwitterFollowCard userName="pheralb" isFollowing>Pablo Hernandez</TwitterFollowCard>
+            {
+                users.map(user => {
+                    const {userName, name, isFollowing} = user
+                    return (
+                        <TwitterFollowCard key={userName} userName={userName} initialIsFollowing={isFollowing}>
+                            {name}
+                        </TwitterFollowCard>
+                    )
+                })
+            }
         </section>
     )
 }
